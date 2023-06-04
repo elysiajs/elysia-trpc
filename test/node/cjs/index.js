4697a3ba999ad0e686428964c9b060848175b10b
bogeychan <bogeychan@outlook.com>
2023-06-04 17:03:45 +0200

    🔧fix: exports in package.json

---
+++ test/node/cjs/index.js
@@ -0,0 +1,11 @@
+if ('Bun' in globalThis) {
+  throw new Error('❌ Use Node.js to run this test!');
+}
+
+const { trpc } = require('@elysiajs/trpc');
+
+if (typeof trpc !== 'function') {
+  throw new Error('❌ CommonJS Node.js failed');
+}
+
+console.log('✅ CommonJS Node.js works!');
